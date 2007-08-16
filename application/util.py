b2807475003f0e812bc15e51d5bd37ce90f1a71d
Dan Pascu <dan@ag-projects.com>
2007-08-16 15:11:29 +0000

    Updated util module description

--- application/util.py
+++ application/util.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2006-2007 Dan Pascu. See LICENSE for details.
 #
 
-"""Miscelaneous application related utilities"""
+"""Miscelaneous utilities"""
 
 __all__ = ['thisHostIP', 'unlink']
 
