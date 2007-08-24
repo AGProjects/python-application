9eda555f08b5c946bc10f36814e5aa6f2f82494f
Dan Pascu <dan@ag-projects.com>
2007-08-24 11:15:51 +0000

    Generalized configuration handling by adding the ConfigFile class
    By adding the ConfigFile class multiple configuration files can be handled
    at the same time. It also makes easier to specify the configuration file
    from the application instead of using the less flexible module attribute
    for the configuration file name. This class obsoletes the previous module
    functions which were removed.

--- application/configuration/__init__.py
+++ application/configuration/__init__.py
@@ -3,7 +3,7 @@
 
 """Application configuration file handling"""
 
-__all__ = ['ConfigSection', 'read_settings', 'get_option', 'get_section', 'dump_settings', 'datatypes']
+__all__ = ['ConfigSection', 'ConfigFile', 'datatypes']
 
 import os
 try:    from ConfigParser import SafeConfigParser as ConfigParser
@@ -15,109 +15,105 @@ from application.process import process
 from application.configuration import datatypes
 
 
-## The configuration name
-name = 'config.ini'
-
-## Context for the log system
-logContext = {'system': 'configuration'}
-
-## The configuration
-Configuration = None
-
-
 class ConfigSection:
     """Defines a section in the configuration file"""
     _dataTypes = {}
 
 
-def _read_configuration():
-    global Configuration
-    Configuration = ConfigParser()
-    files = [os.path.join(path, name) for path in process.get_config_directories() if path is not None]
-    Configuration.read(files)
-
-
-def read_settings(section, object):
-    """Update the object's attributes with values read from the given section in the config"""
-    if Configuration is None:
-        _read_configuration()
-    if section not in Configuration.sections():
-        return
-    for prop in dir(object):
-        if prop[0]=='_':
-            continue
-        ptype = object._dataTypes.get(prop, eval('object.%s.__class__' % prop))
+class ConfigFile(object):
+    """Provide access to a configuration file"""
+    
+    instances = {}
+    log_context = {'system': 'configuration'}
+    
+    def __new__(cls, filename):
+        if not cls.instances.has_key(filename):
+            instance = object.__new__(cls)
+            instance.parser = ConfigParser()
+            files = [os.path.join(path, filename) for path in process.get_config_directories() if path is not None]
+            instance.parser.read(files)
+            cls.instances[filename] = instance
+        return cls.instances[filename]
+    
+    def read_settings(self, section, cls):
+        """Update cls's attributes with values read from the given section"""
+        if not issubclass(cls, ConfigSection):
+            raise TypeError("cls must be a subclass of ConfigSection")
+        if section not in self.parser.sections():
+            return
+        for prop in dir(cls):
+            if prop[0]=='_':
+                continue
+            ptype = cls._dataTypes.get(prop, eval('cls.%s.__class__' % prop))
+            try:
+                val = self.parser.get(section, prop)
+            except:
+                continue
+            else:
+                try:
+                    if ptype is bool:
+                        value = bool(datatypes.Boolean(val))
+                    else:
+                        value = ptype(val)
+                except Exception, why:
+                    msg = "ignoring invalid config value: %s.%s=%s (%s)." % (section, prop, val, why)
+                    log.warn(msg, **ConfigFile.log_context)
+                else:
+                    setattr(cls, prop, value)
+    
+    def get_option(self, section, option, default='', type=str):
+        """Get an option from a given section using type, or default if not found"""
         try:
-            val = Configuration.get(section, prop)
+            value = self.parser.get(section, option)
         except:
-            pass
+            return default
         else:
             try:
-                if ptype is bool:
-                    value = bool(datatypes.Boolean(val))
+                if type is bool:
+                    return bool(datatypes.Boolean(value))
                 else:
-                    value = ptype(val)
+                    return type(value)
             except Exception, why:
-                msg = "ignoring invalid config value: %s.%s=%s (%s)." % (section, prop, val, why)
-                log.warn(msg, **logContext)
-            else:
-                setattr(object, prop, value)
-
-
-def get_option(section, option, default='', type=str):
-    if Configuration is None:
-        _read_configuration()
-    try:
-        value = Configuration.get(section, option)
-    except:
-        return default
-    else:
+                msg = "ignoring invalid config value: %s.%s=%s (%s)." % (section, option, value, why)
+                log.warn(msg, **ConfigFile.log_context)
+                return default
+    
+    def get_section(self, section):
+        """Return a list of tuples with name, value pairs from the section or None if section doesn't exist"""
         try:
-            if type is bool:
-                return bool(datatypes.Boolean(value))
-            else:
-                return type(value)
-        except Exception, why:
-            msg = "ignoring invalid config value: %s.%s=%s (%s)." % (section, option, value, why)
-            log.warn(msg, **logContext)
-            return default
-
-
-def get_section(section):
-    """Return a list of tuples with name, value pairs from the section"""
-    if Configuration is None:
-        _read_configuration()
-    try:
-        return Configuration.items(section)
-    except NoSectionError:
-        return None
-
-
-def dump_settings(object):
-    print '%s:' % object.__name__
-    for x in dir(object):
-        if x[0] == '_': continue
-        print '  %s: %s' % (x, eval('object.%s' % x))
-    print ''
+            return self.parser.items(section)
+        except NoSectionError:
+            return None
+    
+    @staticmethod
+    def dump_settings(cls):
+        print '%s:' % cls.__name__
+        for x in dir(cls):
+            if x[0] == '_': continue
+            print '  %s: %s' % (x, eval('cls.%s' % x))
+        print ''
 
 
 # Backward compatibility functions. Do not use! They will be removed soon.
 #
 import warnings
 
-def readSettings(section, object):
+## The default configuration name used before ConfigFile was introduced
+name = 'config.ini'
+
+def readSettings(section, cls):
     warnings.warn("readSettings was replaced by read_settings", DeprecationWarning, stacklevel=2)
-    return read_settings(section, object)
+    return ConfigFile(name).read_settings(section, cls)
 
 def getOption(section, option, default='', type=str):
     warnings.warn("getOption was replaced by get_option", DeprecationWarning, stacklevel=2)
-    return get_option(section, option, default, type)
+    return ConfigFile(name).get_option(section, option, default, type)
 
 def getSection(section):
     warnings.warn("getSection was replaced by get_section", DeprecationWarning, stacklevel=2)
-    return get_section(section)
+    return ConfigFile(name).get_section(section)
 
-def dumpSettings(object):
+def dumpSettings(cls):
     warnings.warn("dumpSettings was replaced by dump_settings", DeprecationWarning, stacklevel=2)
-    dump_settings(object)
+    ConfigFile.dump_settings(cls)
 
