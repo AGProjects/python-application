6c0d58c2a940478b6dd0d75d315c4b711ed16c8d
Luci Stanescu <luci@ag-projects.com>
2009-07-02 12:33:49 +0000

    Fixed a memory leak caused by ThreadLocal not releasing some internal data

--- application/python/descriptor.py
+++ application/python/descriptor.py
@@ -5,6 +5,7 @@
 
 __all__ = ['ThreadLocal']
 
+import weakref
 from threading import local
 
 class ThreadLocal(object):
@@ -17,14 +18,25 @@ class ThreadLocal(object):
     def __get__(self, obj, objtype):
         if obj is None:
             return self
+        obj_id = id(obj)
+        self_id = id(self)
         try:
-            return self.thread_local.__dict__[(id(obj), id(self))]
+            return self.thread_local.__dict__[(obj_id, self_id)][0]
         except KeyError:
             instance = self.type(*self.args, **self.kw)
-            self.thread_local.__dict__[(id(obj), id(self))] = instance
+            thread_dict = self.thread_local.__dict__
+            ref = weakref.ref(obj, lambda weak_ref: thread_dict.pop((obj_id, self_id)))
+            thread_dict[(obj_id, self_id)] = (instance, ref)
             return instance
     def __set__(self, obj, value):
-        self.thread_local.__dict__[(id(obj), id(self))] = value
+        obj_id = id(obj)
+        self_id = id(self)
+        thread_dict = self.thread_local.__dict__
+        if (obj_id, self_id) in thread_dict:
+            ref = thread_dict[(obj_id, self_id)][1]
+        else:
+            ref = weakref.ref(obj, lambda weak_ref: thread_dict.pop((obj_id, self_id)))
+        thread_dict[(obj_id, self_id)] = (value, ref)
     def __delete__(self, obj):
         raise AttributeError("attribute cannot be deleted")
 
