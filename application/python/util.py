323faacea231ed61aedfd7cef077d417779a5bf2
Dan Pascu <dan@ag-projects.com>
2009-09-03 11:54:57 +0000

    Added indexing support for the Null class

--- application/python/util.py
+++ application/python/util.py
@@ -45,5 +45,8 @@ class Null(object):
     def __getattr__(self, name): return self
     def __setattr__(self, name, value): pass
     def __delattr__(self, name): pass
+    def __getitem__(self, key): return self
+    def __setitem__(self, key, value): pass
+    def __delitem__(self, key): pass
     __str__ = __repr__
 
