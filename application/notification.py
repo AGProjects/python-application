68ba7d25a96058277af167977cd05c3f74aef148
Dan Pascu <dan@ag-projects.com>
2009-11-27 20:12:05 +0000

    Added support for weakly referencing observers in the notification system

--- application/notification.py
+++ application/notification.py
@@ -5,11 +5,13 @@
 
 from __future__ import with_statement
 
-__all__ = ['Any', 'UnknownSender', 'IObserver', 'NotificationData', 'Notification', 'NotificationCenter']
+__all__ = ['Any', 'UnknownSender', 'IObserver', 'NotificationData', 'Notification', 'NotificationCenter', 'ObserverWeakrefProxy']
 
+
+import weakref
 from collections import deque
 from threading import Lock
-from zope.interface import Interface
+from zope.interface import Interface, implements
 
 from application import log
 from application.python.util import Singleton
@@ -41,6 +43,51 @@ class IObserver(Interface):
 
     def handle_notification(notification):
         """Function used to handle a posted Notification"""
+
+
+class ObserverWeakrefProxy(object):
+    """
+    A proxy that allows an observer to be weakly referenced and automatically
+    removes any remaining registrations that the observer didn't clean itself
+    before its reference count dropped to zero.
+    """
+
+    implements(IObserver)
+
+    observer_map = weakref.WeakKeyDictionary()
+    lock = Lock()
+
+    def __new__(cls, observer):
+        if not IObserver.providedBy(observer):
+            raise TypeError("observer must implement the IObserver interface")
+        with cls.lock:
+            if observer in cls.observer_map:
+                return cls.observer_map[observer]
+            instance = object.__new__(cls)
+            instance.observer_ref = weakref.ref(observer, instance.cleanup)
+            instance.tracked_items = set()
+            cls.observer_map[observer] = instance
+            return instance
+
+    def track(self, notification_center, name, sender):
+        observer = self.observer_ref() # retain a reference to avoid threading issues while we add tracking data
+        if observer is not None:       # track data only if observer is alive, else preserve tracked_items as cleanup iterates through them
+            self.tracked_items.add((notification_center, name, sender))
+
+    def untrack(self, notification_center, name, sender):
+        observer = self.observer_ref() # retain a reference to avoid threading issues while we discard tracking data
+        if observer is not None:       # untrack data only if observer is alive, else preserve tracked_items as cleanup iterates through them
+            self.tracked_items.discard((notification_center, name, sender))
+
+    def cleanup(self, ref):
+        # remove all observer's remaining registrations (the ones that the observer didn't remove itself)
+        for notification_center, name, sender in self.tracked_items:
+            notification_center.discard_observer(self, name , sender)
+
+    def handle_notification(self, notification):
+        observer = self.observer_ref()
+        if observer is not None:
+            observer.handle_notification(notification)
 
 
 ## Notification
@@ -109,6 +156,8 @@ class NotificationCenter(object):
             raise TypeError("observer must implement the IObserver interface")
         with self.lock:
             self.observers.setdefault((name, sender), set()).add(observer)
+            if isinstance(observer, ObserverWeakrefProxy):
+                observer.track(self, name, sender)
 
     def remove_observer(self, observer, name=Any, sender=Any):
         """
@@ -128,6 +177,8 @@ class NotificationCenter(object):
                 raise KeyError("observer %r not registered for %r events from %r" % (observer, name, sender))
             if not observer_set:
                 del self.observers[(name, sender)]
+            if isinstance(observer, ObserverWeakrefProxy):
+                observer.untrack(self, name, sender)
 
     def discard_observer(self, observer, name=Any, sender=Any):
         """
@@ -145,6 +196,8 @@ class NotificationCenter(object):
                 observer_set.discard(observer)
                 if not observer_set:
                     del self.observers[(name, sender)]
+                if isinstance(observer, ObserverWeakrefProxy):
+                    observer.untrack(self, name, sender)
 
     def post_notification(self, name, sender=UnknownSender, data=NotificationData()):
         """
