24deeaa2bacab061c7a65957d8742c73c568bc30
Dan Pascu <dan@ag-projects.com>
2011-05-31 07:24:46 +0000

    Added the limit function to restrict a numeric value to a given range

--- application/python/__init__.py
+++ application/python/__init__.py
@@ -3,9 +3,21 @@
 
 """Pyhton utilities and helpers"""
 
-__all__ = ['Null']
+__all__ = ['Null', 'limit']
 
 from application.python.types import NullType
 
 Null = NullType()
+
+try:
+    negative_infinite = float('-infinity')
+    positive_infinite = float('infinity')
+except ValueError:
+    negative_infinite = -1e300000
+    positive_infinite = 1e300000
+
+def limit(value, min=negative_infinite, max=positive_infinite):
+    """Limit a numeric value to the specified range"""
+    from __builtin__ import min as minimum, max as maximum
+    return maximum(min, minimum(value, max))
 
