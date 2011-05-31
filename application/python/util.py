f3878f7ef69b92d443c6cba5c88118822a25a2e8
Dan Pascu <dan@ag-projects.com>
2011-05-31 16:40:09 +0000

    Removed unnecessary code from the fake util module mangling

--- application/python/util.py
+++ application/python/util.py
@@ -7,12 +7,6 @@ __all__ = ['Singleton', 'Null', 'NullType']
 
 import sys
 
-module_name  = __name__
-parent_name, dot, module_basename = __name__.rpartition('.') # this works because we know for sure we have a parent
-
-module = sys.modules[module_name]
-parent = sys.modules[parent_name]
-
 class FakeUtil(object):
     def __init__(self, module):
         self.__module = module
@@ -33,11 +27,10 @@ class FakeUtil(object):
             warnings.warn("importing Null from application.pyhton.util has been deprecated and will be removed in version 1.3.0. Null has been moved to application.python", DeprecationWarning)
         return value
 
-fake_util = FakeUtil(module)
-sys.modules[module_name] = fake_util
-setattr(parent, module_basename, fake_util)
+fake_util = FakeUtil(sys.modules[__name__])
+sys.modules[__name__] = fake_util
 
-del module_name, parent_name, module_basename, module, parent, fake_util, FakeUtil, dot, sys
+del FakeUtil, fake_util, sys
 
 from application.python.types import Singleton, NullTypeMeta, NullType
 from application.python import Null
