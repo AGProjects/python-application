fabe2c7fed073ee73bfa67bbd6d5c63be0d651e6
Dan Pascu <dan@ag-projects.com>
2011-05-31 14:04:27 +0000

    Updated copyright years for the recently modified files

--- application/log/__init__.py
+++ application/log/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2006-2009 Dan Pascu. See LICENSE for details.
+# Copyright (C) 2006-2011 Dan Pascu. See LICENSE for details.
 #
 
 """Application logging system for stdout/stderr and syslog"""
--- application/system.py
+++ application/system.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2006-2007 Dan Pascu. See LICENSE for details.
+# Copyright (C) 2006-2011 Dan Pascu. See LICENSE for details.
 #
 
 """Interaction with the underlying operating system"""
