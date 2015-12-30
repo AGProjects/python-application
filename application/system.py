76d2a91103c44b44de6f55ef58a372088a06f1b4
Dan Pascu <dan@ag-projects.com>
2015-12-30 06:17:53 +0000

    Added openfile (same as open but supports 'x' mode and file permissions)

--- application/system.py
+++ application/system.py
@@ -10,7 +10,7 @@ import socket
 from application.python.types import Singleton
 
 
-__all__ = ['host', 'makedirs', 'unlink']
+__all__ = ['host', 'makedirs', 'openfile', 'unlink', 'FileExistsError']
 
 
 # System properties and attributes
@@ -72,6 +72,59 @@ def makedirs(path, mode=0777):
         if e.errno == errno.EEXIST and os.path.isdir(path) and os.access(path, os.R_OK | os.W_OK | os.X_OK):
             return
         raise
+
+
+class FileExistsError(IOError):
+    pass
+
+
+def openfile(path, mode='r', permissions=0666):
+    if not isinstance(mode, str):
+        raise TypeError('Invalid mode: {!r}'.format(mode))
+    if not set(mode).issubset('rwxabt+'):
+        raise ValueError('Invalid mode: {!r}'.format(mode))
+    if len(set(mode).intersection('rwxa')) != 1 or mode.count('+') > 1:
+        raise ValueError("Must have exactly one of create/read/write/append mode and at most one plus")
+    if 'b' in mode and 't' in mode:
+        raise ValueError("Cannot have both text and binary modes specified at the same time")
+
+    write = append = False
+
+    if 'w' in mode:
+        write = True
+        flags = os.O_CREAT | os.O_TRUNC
+    elif 'x' in mode:
+        write = True
+        flags = os.O_CREAT | os.O_EXCL
+    elif 'a' in mode:
+        write = append = True
+        flags = os.O_CREAT | os.O_APPEND
+    else:
+        flags = 0
+
+    if '+' in mode:
+        flags |= os.O_RDWR
+    elif write:
+        flags |= os.O_WRONLY
+    else:
+        flags |= os.O_RDONLY
+
+    if 'b' in mode:
+        flags |= getattr(os, 'O_BINARY', 0)
+    flags |= getattr(os, 'O_NOINHERIT', 0) or getattr(os, 'O_CLOEXEC', 0)
+
+    try:
+        fd = os.open(path, flags, permissions)
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            raise FileExistsError(*e.args)
+        else:
+            raise IOError(*e.args)
+
+    if append:
+        os.lseek(fd, 0, os.SEEK_END)
+
+    return os.fdopen(fd, mode.replace('x', 'w'))
 
 
 def unlink(path):
