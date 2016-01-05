736610eb4e9c7c4a51739c6023059c4b34ed405e
Dan Pascu <dan@ag-projects.com>
2016-01-05 11:22:51 +0000

    Reordered imports and adjusted spacing

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 
+import os
+
 from distutils.core import setup
 from application import __version__
-import os
+
 
 def find_packages(toplevel):
     return [directory.replace(os.path.sep, '.') for directory, subdirs, files in os.walk(toplevel) if '__init__.py' in files]
+
 
 setup(name         = "python-application",
       version      = __version__,
