e9814489bddcebc7608f9979bae527056637b235
Dan Pascu <dan@ag-projects.com>
2017-06-23 21:08:26 +0000

    Fixed twisted logger and modified it to use the new twisted logger API

--- application/log/extensions/twisted/__init__.py
+++ application/log/extensions/twisted/__init__.py
@@ -6,27 +6,8 @@ import sys
 
 
 def divert_logger():
-    from twisted.python import log
-    import logging
-
-    class SimpleObserver(log.DefaultObserver):
-        """Use logging as log backend for twisted"""
-
-        def _emit(self, record):
-            message = ' '.join(record['message'])
-            if record['isError']:
-                if 'failure' in record:
-                    failure = record['failure']
-                    logging.error(message, exc_info=(type(failure.value), failure.value, failure.tb))
-                else:
-                    logging.error(message)
-            else:
-                logging.info(message)
-
-    if log.defaultObserver is not None:
-        log.defaultObserver.stop()
-        log.defaultObserver = SimpleObserver()
-        log.defaultObserver.start()
+    from twisted.logger import FilteringLogObserver, LogLevel, LogLevelFilterPredicate, STDLibLogObserver, globalLogBeginner
+    globalLogBeginner.beginLoggingTo([FilteringLogObserver(STDLibLogObserver(), [LogLevelFilterPredicate(defaultLogLevel=LogLevel.critical)])], redirectStandardIO=False)
 
 if 'twisted' in sys.modules:
     divert_logger()
