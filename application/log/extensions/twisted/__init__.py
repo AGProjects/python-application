11abd5ba7f5d034f0fcbc347dd86de76957522fb
Dan Pascu <dan@ag-projects.com>
2017-06-24 11:46:54 +0000

    Silence warning about package requirement, as it's not really a requirement

--- application/log/extensions/twisted/__init__.py
+++ application/log/extensions/twisted/__init__.py
@@ -6,6 +6,7 @@ import sys
 
 
 def divert_logger():
+    # noinspection PyPackageRequirements
     from twisted.logger import FilteringLogObserver, LogLevel, LogLevelFilterPredicate, STDLibLogObserver, globalLogBeginner
     globalLogBeginner.beginLoggingTo([FilteringLogObserver(STDLibLogObserver(), [LogLevelFilterPredicate(defaultLogLevel=LogLevel.critical)])], redirectStandardIO=False)
 
@@ -13,4 +14,3 @@ if 'twisted' in sys.modules:
     divert_logger()
 else:
     sys.path.insert(0, os.path.realpath(__path__[0]))
-
