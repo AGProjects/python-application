1eec333277d5bc931b9ee6bc2408683f6111fdfd
Dan Pascu <dan@ag-projects.com>
2017-06-24 11:42:06 +0000

    Removed unnecessary name aliasing on import

--- application/log/extensions/twisted/twisted.py
+++ application/log/extensions/twisted/twisted.py
@@ -1,17 +1,18 @@
 
 from __future__ import absolute_import
 
-from application.log.extensions import twisted as fake_twisted
 import os
 import sys
 
+from application.log.extensions import twisted
+
 try:
-    sys.path.remove(os.path.realpath(fake_twisted.__path__[0]))
+    sys.path.remove(os.path.realpath(twisted.__path__[0]))
 except ValueError:
     pass
 else:
     sys.modules.pop('twisted', None)
+    # noinspection PyPackageRequirements
     import twisted
     from application.log.extensions.twisted import divert_logger
     divert_logger()
-
