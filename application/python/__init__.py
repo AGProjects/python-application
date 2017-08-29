72d1e9e268003cb7411726c7ece1e68c4f38e9a7
Dan Pascu <dan@ag-projects.com>
2017-08-29 10:54:53 +0000

    Added subclasses function in application.python

--- application/python/__init__.py
+++ application/python/__init__.py
@@ -3,9 +3,10 @@
 
 from __builtin__ import min as minimum, max as maximum
 from application.python.types import NullType
+from collections import deque
 
 
-__all__ = 'Null', 'limit'
+__all__ = 'Null', 'limit', 'subclasses'
 
 
 Null = NullType()
@@ -22,3 +23,12 @@ except ValueError:
 def limit(value, min=negative_infinite, max=positive_infinite):
     """Limit a numeric value to the specified range"""
     return maximum(min, minimum(value, max))
+
+
+def subclasses(cls):
+    """Recursively find all the subclasses of a given class"""
+    classes = deque(cls.__subclasses__())
+    while classes:
+        subclass = classes.popleft()
+        classes.extend(subclass.__subclasses__())
+        yield subclass
